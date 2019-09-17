772f75c9b83f8aeb31f062f4a417d5a78df93af0
Gary <garyyue2017@gmail.com>
2019-09-17 02:25:14 +0000

    finished stretch work

--- lib/store.rb
+++ lib/store.rb
@@ -4,9 +4,17 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { :greater_than_or_equal_to => 0 }
   validate :carries_mens_or_womens_apparel
 
+  before_destroy :ensure_no_employees
+
+  private
   def carries_mens_or_womens_apparel
     if !mens_apparel && !womens_apparel
       errors.add(:mens_apparel, "must contain mens or womens apparel")
     end
+  end
+
+  def ensure_no_employees
+    puts employees.any?
+    return false if employees.any?
   end
 end
